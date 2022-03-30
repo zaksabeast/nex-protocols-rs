d3af7f3008c6bf430ba6c6369cf02bac1d299eee
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2022-03-30 07:35:12 -0500

    Split datastore usum types and protocol

--- src/datastore_usum/types.rs
+++ src/datastore_usum/types.rs
@@ -1,6 +1,4 @@
-use nex_rs::client::ClientConnection;
 use nex_rs::nex_types::{DateTime, NexList, NexQBuffer, NexString, ResultCode, ResultRange};
-use nex_rs::packet::PacketV1;
 use no_std_io::{EndianRead, EndianWrite};
 
 #[derive(Debug, EndianRead, EndianWrite)]
@@ -360,82 +358,4 @@ struct GlobalTradeStationSearchPokemonResult {
 #[derive(Debug, EndianRead, EndianWrite)]
 struct SearchPokemonV2Response {
     p_result: GlobalTradeStationSearchPokemonResult,
-}
-
-pub const DATASTORE_PROTOCOL_ID: u8 = 0x73;
-
-pub trait DataStoreProtocol {
-    fn get_metas(&self, client: &mut ClientConnection) -> Result<(), &'static str>;
-    fn rate_object(&self, client: &mut ClientConnection) -> Result<(), &'static str>;
-    fn post_meta_binary(&self, client: &mut ClientConnection) -> Result<(), &'static str>;
-    fn change_metas(&self, client: &mut ClientConnection) -> Result<(), &'static str>;
-    fn prepare_upload_pokemon(&self, client: &mut ClientConnection) -> Result<(), &'static str>;
-    fn upload_pokemon(&self, client: &mut ClientConnection) -> Result<(), &'static str>;
-    fn prepare_trade_pokemon(&self, client: &mut ClientConnection) -> Result<(), &'static str>;
-    fn trade_pokemon(&self, client: &mut ClientConnection) -> Result<(), &'static str>;
-    fn download_other_pokemon(&self, client: &mut ClientConnection) -> Result<(), &'static str>;
-    fn download_my_pokemon(&self, client: &mut ClientConnection) -> Result<(), &'static str>;
-    fn delete_pokemon(&self, client: &mut ClientConnection) -> Result<(), &'static str>;
-    fn search_pokemon_v2(&self, client: &mut ClientConnection) -> Result<(), &'static str>;
-
-    fn handle_get_metas(
-        &self,
-        client: &mut ClientConnection,
-        packet: &PacketV1,
-    ) -> Result<(), &'static str>;
-    fn handle_rate_object(
-        &self,
-        client: &mut ClientConnection,
-        packet: &PacketV1,
-    ) -> Result<(), &'static str>;
-    fn handle_post_meta_binary(
-        &self,
-        client: &mut ClientConnection,
-        packet: &PacketV1,
-    ) -> Result<(), &'static str>;
-    fn handle_change_metas(
-        &self,
-        client: &mut ClientConnection,
-        packet: &PacketV1,
-    ) -> Result<(), &'static str>;
-    fn handle_prepare_upload_pokemon(
-        &self,
-        client: &mut ClientConnection,
-        packet: &PacketV1,
-    ) -> Result<(), &'static str>;
-    fn handle_upload_pokemon(
-        &self,
-        client: &mut ClientConnection,
-        packet: &PacketV1,
-    ) -> Result<(), &'static str>;
-    fn handle_prepare_trade_pokemon(
-        &self,
-        client: &mut ClientConnection,
-        packet: &PacketV1,
-    ) -> Result<(), &'static str>;
-    fn handle_trade_pokemon(
-        &self,
-        client: &mut ClientConnection,
-        packet: &PacketV1,
-    ) -> Result<(), &'static str>;
-    fn handle_download_other_pokemon(
-        &self,
-        client: &mut ClientConnection,
-        packet: &PacketV1,
-    ) -> Result<(), &'static str>;
-    fn handle_download_my_pokemon(
-        &self,
-        client: &mut ClientConnection,
-        packet: &PacketV1,
-    ) -> Result<(), &'static str>;
-    fn handle_delete_pokemon(
-        &self,
-        client: &mut ClientConnection,
-        packet: &PacketV1,
-    ) -> Result<(), &'static str>;
-    fn handle_search_pokemon_v2(
-        &self,
-        client: &mut ClientConnection,
-        packet: &PacketV1,
-    ) -> Result<(), &'static str>;
 }
