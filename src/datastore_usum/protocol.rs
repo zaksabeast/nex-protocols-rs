f5ca365a7731ae263bcf4922c9e28dd6daa36d7c
Steve Cook <steviecook210@gmail.com>
2022-03-30 10:25:02 -0400

    Add DataStore USUM method ids

--- src/datastore_usum/protocol.rs
+++ src/datastore_usum/protocol.rs
@@ -1,7 +1,25 @@
 use nex_rs::client::ClientConnection;
 use nex_rs::packet::PacketV1;
+use num_enum::{IntoPrimitive, TryFromPrimitive};
 
 pub const DATASTORE_PROTOCOL_ID: u8 = 0x73;
+
+#[derive(Debug, Clone, Copy, PartialEq, TryFromPrimitive, IntoPrimitive)]
+#[repr(u32)]
+pub enum DataStoreMethod {
+    GetMetas = 0x9,
+    RateObject = 0xF,
+    PostMetaBinary = 0x15,
+    ChangeMetas = 0x27,
+    PrepareUploadPokemon = 0x2F,
+    UploadPokemon = 0x30,
+    PrepareTradePokemon = 0x32,
+    TradePokemon = 0x33,
+    DownloadOtherPokemon = 0x34,
+    DownloadMyPokemon = 0x35,
+    DeletePokemon = 0x36,
+    SearchPokemonV2 = 0x37,
+}
 
 pub trait DataStoreProtocol {
     fn get_metas(&self, client: &mut ClientConnection) -> Result<(), &'static str>;
